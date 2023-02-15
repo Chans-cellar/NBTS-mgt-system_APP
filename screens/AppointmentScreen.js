15ac1dbb0c7adc69cd2f18088c6f84c46d1e8deb
Luvi-Stacks <ict19870@fot.sjp.ac.lk>
2023-02-15 14:24:29 +0530

    README.md updated.

---
+++ screens/AppointmentScreen.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {} from './screens'
+
+export default function AppointmentScreen() {
+    return (
+        <View style={styles.container}>
+
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
