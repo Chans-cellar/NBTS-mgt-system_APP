006d99839546369e148ee6e39c86a06f4246d6bb
Luvi-Stacks <ict19870@fot.sjp.ac.lk>
2023-02-15 16:00:45 +0530

    README.md updated.

--- screens/AlertScreen.js
+++ screens/AlertScreen.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {} from './screens'
+import {COLORS, FONTS} from "../Theme";
+import AlertNotification from "../Components/AlertNotification";
 
 export default function AlertScreen() {
     return (
         <View style={styles.container}>
+            <View style={styles.headingDiv}>
+                <Text style={styles.headingDiv__Main}>
+                    Notifications
+                </Text>
+                <Text style={styles.headingDiv__Sub}>
+                    Recent Alerts and Notifications
+                </Text>
+
+            </View>
+
+            <View style={styles.alertDiv}>
+                <AlertNotification/>
+            </View>
+
 
         </View>
     );
@@ -13,8 +28,30 @@ export default function AlertScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: COLORS.LavenderBlush,
         alignItems: 'center',
-        justifyContent: 'center',
     },
+
+    headingDiv:{
+        paddingTop:70,
+        paddingLeft:30,
+        alignSelf:"flex-start"
+
+
+    },
+
+    alertDiv:{
+        marginVertical:20
+
+    },
+    headingDiv__Main:{
+        fontFamily:FONTS.Black,
+        color: COLORS.WineRed,
+        fontSize:24
+    },
+    headingDiv__Sub:{
+        fontFamily:FONTS.Bold,
+        color: COLORS.AshPink
+
+    }
 });
